85bb94328af585f62b8cbabc7287efcbfa1460a1
Kevin Baugh <loraxx753@gmail.com>
2012-02-16 14:15:59 -0500

    Almost have it working

--- index.php
+++ index.php
@@ -13,11 +13,21 @@
 var voter = {};
 
 $(document).ready(function() {
-	$("#voterIdSubmit").click(function() {
+	$("#voterIdSubmit").tap(function() {
 		var voterId = $("#voterId").val();
 		if(voterId.match(/^[0-9]+$/) && voterId > 1000 && voterId < 1050)
 		{
-			$.mobile.changePage( "#two" );	
+			$.post('/api/vote/check/', { 'voter' : voter}, function(data) {
+				if(data.success)
+				{
+					alert("You've already voted!");
+				}
+				else
+				{
+					voter.id = voterId;
+					$.mobile.changePage( "#two" );			
+				}
+			}, "json");
 		}
 		else
 		{
@@ -36,10 +46,76 @@ $(document).ready(function() {
 	        $('#about-content li').html('Votes: '+candidate.votes);
 			$.mobile.changePage( "#about" );	
 	    }, "json");
+	    return false;
+	});
+	$('#castVote').tap(function(e) {
+		e.preventDefault();
+		voter.vote = $("#ballot").find("input:checked").attr("value")
+       $.post('/api/vote/add/', { 'voter' : voter }, function(data) {
+            //If the vote has been successfully cast, send them to the voted page.
+            if(data.success)
+            {
+				$.mobile.changePage( "#results", { transition: "slideDown"} );	
+
+            }
+            //An extra check to make sure they haven't voted yet, just to be sure.
+            else
+            {
+                alert("You've already voted");
+            }
+        }, "json");    
 	});
 });
 
 </script>
+<style type="text/css">
+	#results div ul {
+		margin: 0;
+		padding: 0;
+	}
+	#results div ul li {
+		list-style: none;
+		height: 50px;
+		margin: 10px;
+	}
+	#results div .votes {
+		display: inline-block;
+		height: 10px;
+		background: #f00;
+		width: 1%;
+		border-radius: 10px;
+		background-image: linear-gradient(bottom, rgb(196,27,8) 26%, rgb(227,52,32) 63%, rgb(232,49,21) 82%);
+		background-image: -o-linear-gradient(bottom, rgb(196,27,8) 26%, rgb(227,52,32) 63%, rgb(232,49,21) 82%);
+		background-image: -moz-linear-gradient(bottom, rgb(196,27,8) 26%, rgb(227,52,32) 63%, rgb(232,49,21) 82%);
+		background-image: -webkit-linear-gradient(bottom, rgb(196,27,8) 26%, rgb(227,52,32) 63%, rgb(232,49,21) 82%);
+		background-image: -ms-linear-gradient(bottom, rgb(196,27,8) 26%, rgb(227,52,32) 63%, rgb(232,49,21) 82%);
+
+		background-image: -webkit-gradient(
+			linear,
+			left bottom,
+			left top,
+			color-stop(0.26, rgb(196,27,8)),
+			color-stop(0.63, rgb(227,52,32)),
+			color-stop(0.82, rgb(232,49,21))
+		);
+	}
+	#results div .inline
+	{
+		padding: 0;
+		margin-top: -10px;
+		width: 200px;
+		display: inline-block;
+	}
+	#results div .percent
+	{
+		padding: 0;
+		margin: 0;
+		display: block;
+		vertical-align: 0px;
+	}
+
+</style>
+
 </head>
 
 <body>
@@ -63,14 +139,16 @@ $(document).ready(function() {
 
 
 <!-- Start of second page: #two -->
-<div data-role="page" id="two">	<div data-role="header">
+<div data-role="page" id="two">	
+	<div data-role="header">
 		<h1>About Each Candidate</h1>
 	</div><!-- /header -->
 	<ul data-role="listview" data-theme="g">
 		<?php foreach ($candidates as $runner) { ?>
 		<li><a class="candidateLink" data-candidate="<?php Load::link($runner); ?>" href="#about"><?php Load::icon($runner); ?> <?php Load::name($runner); ?></a></li>
 		<?php } ?>
-	</ul>	
+	</ul>
+	<p><a data-role="button" href="#vote">Continue on to Cast Your Vote</a></p>
 	<div data-role="footer">
 		<h4>Page Footer</h4>
 	</div><!-- /footer -->
@@ -98,6 +176,48 @@ $(document).ready(function() {
 		<h4>Page Footer</h4>
 	</div><!-- /footer -->
 </div><!-- /page popup -->
+<div data-role="page" id="vote">
 
+	<div data-role="header">
+		<h1>Cast Your Vote</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">				
+		<div id="ballot" data-role="fieldcontain">
+					<?php 
+						$count = 0;
+						foreach($candidates as $running) { ?>
+					<input type="radio" name="voter" id="checkbox-<?php echo $count; ?>" class="custom" value="<?php Load::id($running); ?>" />
+					<label for="checkbox-<?php echo $count; ?>"><?php Load::icon($running); ?> Vote For <?php Load::name($running); ?></label>
+					<?php $count++; } ?>
+		</div>
+		<p><a id="castVote" data-role="button">Click to Cast Your Vote</a></p>
+	</div>
+	<div data-role="footer">
+		<h4>Page Footer</h4>
+	</div><!-- /footer -->
+</div><!-- /page popup -->
+<div data-role="page" id="results">
+
+	<div data-role="header">
+		<h1>Cast Your Vote</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">				
+		<ul>
+		<?php foreach($candidates as $name) { ?>
+			<li>
+				<ul id="<?php Load::link($name); ?>" class="candidate">
+					<li><?php Load::icon($name); ?>
+					<div class="inline"><span class="percent">0%</span> <span class="votes"></span></div>
+				</ul>
+			</li>
+		<?php } ?>
+		</ul>
+	</div>
+	<div data-role="footer">
+		<h4>Page Footer</h4>
+	</div><!-- /footer -->
+</div>
 </body>
 </html>
