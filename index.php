1089800e278145afaa2cfde1315d4ea3a35077b3
Kevin Baugh <loraxx753@gmail.com>
2012-02-16 14:48:42 -0500

    Changed DTD and header info to Jquery Mobile/HTML5

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php require_once("config/config.php"); ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html> 
+<html class="ui-mobile-rendering"> 
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
 <title>Project 02</title>
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
 <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
